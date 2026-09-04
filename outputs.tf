ef5efc838cbb4d41c4dff6940faabbe88b91034c
jin3055-Beep <jin3055@kookmin.ac.kr>
2026-09-04 14:39:47 +0900

    Revert "test: add EC2 RDS and ALB assessment targets (#33)" (#34)
    This reverts commit d32e969a13e7d85fc187266e68a7cf2583e1f17c.

--- outputs.tf
+++ outputs.tf
@@ -7,16 +7,3 @@ output "sandbox_bucket_arn" {
   description = "ARN of the customer-controlled onboarding bucket."
   value       = aws_s3_bucket.sandbox.arn
 }
-
-output "assessment_resources" {
-  description = "Resource coordinates to copy into the platform's approved assessment target."
-  value = [
-    { resource_type = "AWS::S3::Bucket", resource_id = aws_s3_bucket.sandbox.id },
-    { resource_type = "AWS::EC2::Instance", resource_id = aws_instance.assessment.id },
-    { resource_type = "AWS::RDS::DBInstance", resource_id = aws_db_instance.assessment.identifier },
-    {
-      resource_type = "AWS::ElasticLoadBalancingV2::LoadBalancer"
-      resource_id   = aws_lb.assessment.arn
-    },
-  ]
-}
